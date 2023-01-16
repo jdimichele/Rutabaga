b5a7f231abb68040ba5c5cabbddfa53faa28820a
jdimichele <justindimichele@gmail.com>
2023-01-16 15:26:13 -0800

    Minor additions before pushing to main

--- src/pages/subpages/RecipeDetails.vue
+++ src/pages/subpages/RecipeDetails.vue
@@ -2,12 +2,14 @@
   <ion-page>
     <base-header title="Recipe Details"></base-header>
     <ion-content v-if="currentRecipe">
-      <!-- <div v-if="isLoading">
-      <ion-loading></ion-loading>
-    </div> -->
-
       <div>
         <h2>{{ this.currentRecipe[0].recipeName }}</h2>
+        <h2>{{ this.currentRecipe[0].recipePhoto }}</h2>
+        <h2>{{ this.currentRecipe[0].recipeTime }}</h2>
+        <h2>{{ this.currentRecipe[0].recipeServings }}</h2>
+        <h2>{{ this.currentRecipe[0].recipeCategory }}</h2>
+        <h2>{{ this.currentRecipe[0].recipeIngredients }}</h2>
+        <h2>{{ this.currentRecipe[0].recipeInstructions }}</h2>
       </div>
     </ion-content>
   </ion-page>
