5cd305cb116b319fb67a4c6c3e790625a54838a1
jdimichele <justindimichele@gmail.com>
2023-02-26 20:24:03 -0800

    Got rid of them nasty CSS properties and just used Ionic for grid control instead.

--- src/pages/subpages/RecipeDetails.vue
+++ src/pages/subpages/RecipeDetails.vue
@@ -3,62 +3,109 @@
     <base-header title="Recipe Details"></base-header>
     <ion-content>
       <base-card v-if="currentRecipe">
-        <div class="recipeInfo">
-          <ion-item lines="none" class="recipePhoto">
-            <img :src="this.currentRecipe[0].recipePhoto" />
-          </ion-item>
-          <div class="recipeTitle">
-            <ion-item lines="none">
-              <h1 class="recipeName">{{ this.currentRecipe[0].recipeName }}</h1>
-            </ion-item>
-            <ion-badge class="recipeBadge">
-              {{ this.currentRecipe[0].recipeCategory }}
-            </ion-badge>
-            <ion-item lines="none" class="recipeTime">
-              {{ this.currentRecipe[0].recipeTime }} minutes
-            </ion-item>
-            <ion-item lines="none" class="recipeServings">
-              {{ this.currentRecipe[0].recipeServings }} servings
-            </ion-item>
-          </div>
-          <div class="recipeIngredients">
-            <h6>Ingredients:</h6>
-            <ion-list v-for="(item, index) in currentIngredients" :key="index">
-              <ion-item lines="none">
-                <li>
-                  {{ this.currentRecipe[0].recipeIngredients[index].qty }}
-                  {{ this.currentRecipe[0].recipeIngredients[index].unit }}
-                  {{ this.currentRecipe[0].recipeIngredients[index].name }}
-                </li>
+        <ion-grid>
+          <div class="recipeInfo">
+            <ion-row>
+              <ion-item lines="none" class="recipePhoto">
+                <img :src="this.currentRecipe[0].recipePhoto" />
               </ion-item>
-            </ion-list>
+            </ion-row>
+            <div class="recipeTitle">
+              <ion-row>
+                <ion-col>
+                  <ion-item lines="none">
+                    <h1 class="recipeName">
+                      {{ this.currentRecipe[0].recipeName }}
+                    </h1>
+                  </ion-item>
+                </ion-col>
+                <ion-col>
+                  <ion-badge class="recipeBadge">
+                    {{ this.currentRecipe[0].recipeCategory }}
+                  </ion-badge>
+                </ion-col>
+                <ion-item lines="none" class="recipeTime">
+                  {{ this.currentRecipe[0].recipeTime }} minutes
+                </ion-item>
+                <ion-item lines="none" class="recipeServings">
+                  {{ this.currentRecipe[0].recipeServings }} servings
+                </ion-item>
+              </ion-row>
+            </div>
+            <ion-row>
+              <ion-col>
+                <div class="recipeIngredients">
+                  <h6>Ingredients:</h6>
+                  <ion-list
+                    v-for="(item, index) in currentIngredients"
+                    :key="index"
+                  >
+                    <ion-item lines="none">
+                      <li>
+                        {{ this.currentRecipe[0].recipeIngredients[index].qty }}
+                        {{
+                          this.currentRecipe[0].recipeIngredients[index].unit
+                        }}
+                        {{
+                          this.currentRecipe[0].recipeIngredients[index].name
+                        }}
+                      </li>
+                    </ion-item>
+                  </ion-list>
+                </div>
+              </ion-col>
+              <ion-col>
+                <div class="recipeInstructions">
+                  <h6>Step-By-Step:</h6>
+                  <ion-list
+                    v-for="(item, index) in currentInstructions"
+                    :key="index"
+                  >
+                    <ion-item lines="none">
+                      <h6 class="pr-2">Step {{ index + 1 }}:</h6>
+                      <p>
+                        {{
+                          this.currentRecipe[0].recipeInstructions[index]
+                            .instruction
+                        }}
+                      </p>
+                    </ion-item>
+                  </ion-list>
+                </div>
+              </ion-col>
+            </ion-row>
           </div>
-          <div class="recipeInstructions">
-            <h6>Step-By-Step:</h6>
-            <ion-list v-for="(item, index) in currentInstructions" :key="index">
-              <ion-item lines="none">
-                <h6 class="pr-2">Step {{ index + 1 }}:</h6>
-                <p>
-                  {{
-                    this.currentRecipe[0].recipeInstructions[index].instruction
-                  }}
-                </p>
-              </ion-item>
-            </ion-list>
-          </div>
-        </div>
+        </ion-grid>
       </base-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonContent, IonItem, IonList, IonBadge } from "@ionic/vue";
+import {
+  IonPage,
+  IonContent,
+  IonItem,
+  IonList,
+  IonBadge,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/vue";
 import { mapGetters } from "vuex";
 
 export default {
   name: "RecipeDetails",
-  components: { IonPage, IonContent, IonItem, IonList, IonBadge },
+  components: {
+    IonPage,
+    IonContent,
+    IonItem,
+    IonList,
+    IonBadge,
+    IonGrid,
+    IonRow,
+    IonCol,
+  },
   data() {
     return {
       isLoading: false,
@@ -82,18 +129,12 @@ export default {
 </script>
 
 <style scoped>
-.recipeInfo {
-  display: grid;
-  grid-template-columns: auto auto auto auto;
-}
 .recipePhoto {
   width: 100%;
   border-radius: 0.5rem 0.5rem 0 0;
   max-height: fit-content;
-  grid-area: 1 / 1 / span 3 / span 4;
 }
 .recipeTitle {
-  grid-area: 5 / 1 / span 1 / span 4;
   display: inline-flex;
 }
 .recipeBadge {
@@ -101,11 +142,5 @@ export default {
   position: absolute;
   height: 23px;
   right: 0px;
-}
-.recipeIngredients {
-  grid-area: 6 / 2 / span 1 / span 1;
-}
-.recipeInstructions {
-  grid-area: 6 / 3 / span 1 / span 1;
 }
 </style>
