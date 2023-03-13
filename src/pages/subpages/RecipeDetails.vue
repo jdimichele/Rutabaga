2ac474a353594c5ffd6bc0b75fe1ed70b6ddd7be
jdimichele <justindimichele@gmail.com>
2023-03-12 21:08:52 -0700

    Better sizing for columns for both desktop and mobile.

--- src/pages/subpages/RecipeDetails.vue
+++ src/pages/subpages/RecipeDetails.vue
@@ -4,45 +4,39 @@
     <ion-content>
       <base-card v-if="currentRecipe">
         <ion-grid>
-          <div class="recipeInfo">
-            <ion-row>
-              <ion-item lines="none" class="recipePhoto">
-                <img :src="this.currentRecipe[0].recipePhoto" />
-              </ion-item>
-            </ion-row>
-            <div class="recipeTitle">
-              <ion-row>
-                <ion-col size-md="6" size-sm="1">
+          <ion-row>
+            <ion-item lines="none" class="recipePhoto">
+              <img :src="this.currentRecipe[0].recipePhoto" />
+            </ion-item>
+          </ion-row>
+          <ion-row class="ion-justify-content-start">
+            <ion-item lines="none">
+              <h1 class="recipeName">
+                {{ this.currentRecipe[0].recipeName }}
+              </h1>
+            </ion-item>
+          </ion-row>
+          <ion-row>
+            <ion-item lines="none" class="recipeTime">
+              {{ this.currentRecipe[0].recipeTime }} minutes
+            </ion-item>
+            <ion-item lines="none" class="recipeServings">
+              {{ this.currentRecipe[0].recipeServings }} servings
+            </ion-item>
+            <ion-badge class="recipeBadge">
+              {{ this.currentRecipe[0].recipeCategory }}
+            </ion-badge>
+          </ion-row>
+          <ion-row>
+            <ion-col size-md="6" size-sm="1">
+              <div class="recipeIngredients">
+                <h6>Ingredients:</h6>
+                <ion-list
+                  v-for="(item, index) in currentIngredients"
+                  :key="index"
+                >
                   <ion-item lines="none">
-                    <h1 class="recipeName">
-                      {{ this.currentRecipe[0].recipeName }}
-                    </h1>
-                  </ion-item>
-                </ion-col>
-                <ion-row>
-                  <ion-item lines="none" class="recipeTime">
-                    {{ this.currentRecipe[0].recipeTime }} minutes
-                  </ion-item>
-                  <ion-item lines="none" class="recipeServings">
-                    {{ this.currentRecipe[0].recipeServings }} servings
-                  </ion-item>
-                </ion-row>
-                <ion-col size-md="6" size-sm="1">
-                  <ion-badge class="recipeBadge">
-                    {{ this.currentRecipe[0].recipeCategory }}
-                  </ion-badge>
-                </ion-col>
-              </ion-row>
-            </div>
-            <ion-row>
-              <ion-col size-md="6" size-sm="1">
-                <div class="recipeIngredients">
-                  <h6>Ingredients:</h6>
-                  <ion-list
-                    v-for="(item, index) in currentIngredients"
-                    :key="index"
-                  >
-                    <ion-item lines="none">
+                    <ul class="iList">
                       <li>
                         {{ this.currentRecipe[0].recipeIngredients[index].qty }}
                         {{
@@ -52,31 +46,29 @@
                           this.currentRecipe[0].recipeIngredients[index].name
                         }}
                       </li>
-                    </ion-item>
-                  </ion-list>
-                </div>
-              </ion-col>
-              <ion-col size-md="6" size-sm="1">
-                <div class="recipeInstructions">
-                  <h6>Step-By-Step:</h6>
-                  <ion-list
-                    v-for="(item, index) in currentInstructions"
-                    :key="index"
-                  >
-                    <ion-item lines="none">
-                      <h6 class="pr-2">Step {{ index + 1 }}:</h6>
-                      <p class="">
-                        {{
-                          this.currentRecipe[0].recipeInstructions[index]
-                            .instruction
-                        }}
-                      </p>
-                    </ion-item>
-                  </ion-list>
-                </div>
-              </ion-col>
-            </ion-row>
-          </div>
+                    </ul>
+                  </ion-item>
+                </ion-list>
+              </div>
+            </ion-col>
+            <ion-col size-md="6" size-sm="1">
+              <div class="recipeInstructions">
+                <h6>Step-By-Step:</h6>
+                <ion-list
+                  v-for="(item, index) in currentInstructions"
+                  :key="index"
+                >
+                  <ion-item lines="none">
+                    <h6 class="pr-2">Step {{ index + 1 }}:</h6>
+                    {{
+                      this.currentRecipe[0].recipeInstructions[index]
+                        .instruction
+                    }}
+                  </ion-item>
+                </ion-list>
+              </div>
+            </ion-col>
+          </ion-row>
         </ion-grid>
       </base-card>
     </ion-content>
@@ -133,7 +125,6 @@ export default {
 <style scoped>
 .recipePhoto {
   width: 100%;
-  border-radius: 0.5rem 0.5rem 0 0;
   max-height: fit-content;
 }
 .recipeTitle {
@@ -147,5 +138,8 @@ export default {
   position: absolute;
   height: 23px;
   right: 0px;
+}
+ul {
+  list-style-type: circle;
 }
 </style>
