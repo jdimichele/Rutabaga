dbc57069ca700538748cd6ef99c2e067abb139c1
jdimichele <justindimichele@gmail.com>
2023-05-15 17:30:38 -0700

    Moved the SearchPage logic to new Vue component.

--- src/pages/main/SearchPage.vue
+++ src/pages/main/SearchPage.vue
@@ -2,59 +2,22 @@
   <ion-page>
     <base-header title="Search"></base-header>
     <ion-content :fullscreen="true">
-      <ion-searchbar
-        class="py-10"
-        mode="ios"
-        v-model="search"
-        placeholder="Time to find that perfect recipe..."
-      ></ion-searchbar>
-      <div>
-        <ul>
-          <li v-if="searchRecipes.length === 1">
-            {{ searchRecipes[0].recipeName }}
-          </li>
-          <li
-            v-else
-            v-for="recipe in searchRecipes"
-            :key="recipe.recipeID"
-            @click="openRecipeDetails(recipe.recipeID)"
-          >
-            {{ recipe.recipeName }}
-          </li>
-        </ul>
-      </div>
+      <search-recipes-item></search-recipes-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonContent, IonSearchbar } from "@ionic/vue";
+import { IonPage, IonContent } from "@ionic/vue";
 import BaseHeader from "../../components/ui/BaseHeader.vue";
+import SearchRecipesItem from "../../components/recipes/SearchRecipesItem.vue";
 
 export default {
   components: {
     IonContent,
     IonPage,
-    IonSearchbar,
     BaseHeader,
-  },
-  data() {
-    return {
-      search: "",
-    };
-  },
-  methods: {
-    openRecipeDetails(targetID) {
-      this.$router.push({
-        name: "RecipeDetails",
-        params: { id: targetID },
-      });
-    },
-  },
-  computed: {
-    searchRecipes() {
-      return this.$store.getters["recipes/getRecipeByName"](this.search);
-    },
+    SearchRecipesItem,
   },
 };
 </script>
