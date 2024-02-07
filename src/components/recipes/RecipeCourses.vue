c10479c52f45320ea6a56a5c6c827a0f0d4c65d9
jdimichele <justindimichele@gmail.com>
2024-02-06 19:37:28 -0800

    AllRecipes page now loads Categories entered by users, along with an error message if there are no categories.

--- src/components/recipes/RecipeCourses.vue
+++ src/components/recipes/RecipeCourses.vue
@@ -2,56 +2,14 @@
   <base-card>
     <ion-title class="my-2.5 text-center">Courses:</ion-title>
     <ion-accordion-group>
-      <ion-accordion>
-        <recipe-course-item :courseName="'Appetizers'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Breakfast'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Desserts'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Dinner'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Drinks'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Lunch'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Sauces'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Sides'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Snacks'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Soups'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Staples'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Vegan'"></recipe-course-item>
-      </ion-accordion>
-
-      <ion-accordion>
-        <recipe-course-item :courseName="'Vegetarian'"></recipe-course-item>
+      <div v-show="getCategories">
+        <h3 class="text-center py-10">
+          You need to add some categories for your recipes. Click on the gear at
+          the top right and click 'Configure' to add some courses and categories!
+        </h3>
+      </div>
+      <ion-accordion v-for="category in getCategories" :key="category">
+        <recipe-course-item :courseName="`${category}`"></recipe-course-item>
       </ion-accordion>
     </ion-accordion-group>
   </base-card>
@@ -59,6 +17,7 @@
 
 <script>
 import { IonAccordion, IonAccordionGroup, IonTitle } from "@ionic/vue";
+import { mapGetters } from "vuex";
 import RecipeCourseItem from "./RecipeCourseItem.vue";
 
 export default {
@@ -67,6 +26,9 @@ export default {
     IonAccordionGroup,
     IonTitle,
     RecipeCourseItem,
+  },
+  computed: {
+    ...mapGetters("recipes", ["getCategories"]),
   },
 };
 </script>
