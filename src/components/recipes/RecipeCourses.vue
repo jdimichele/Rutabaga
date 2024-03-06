2aa6b153f22d9cc3c4f6d3a733b73872e0f6dfb6
jdimichele <justindimichele@gmail.com>
2024-03-05 22:01:35 -0800

    Minor change to Courses

--- src/components/recipes/RecipeCourses.vue
+++ src/components/recipes/RecipeCourses.vue
@@ -2,14 +2,22 @@
   <base-card>
     <ion-title class="my-2.5 text-center">Courses:</ion-title>
     <ion-accordion-group>
-      <div v-show="getCategories">
+      <div v-if="hasCourses">
         <h3 class="text-center py-10">
-          You need to add some categories for your recipes. Click on the gear at
-          the top right and click 'Configure' to add some courses and categories!
+          You need to add some courses for your recipes. Click on the gear at
+          the top right and click 'Configure' to add some courses and
+          categories!
         </h3>
       </div>
-      <ion-accordion v-for="category in getCategories" :key="category">
-        <recipe-course-item :courseName="`${category}`"></recipe-course-item>
+      <div>
+        <ul>
+          <li v-for="course in getCourses" :key="course">
+            {{ course }}
+          </li>
+        </ul>
+      </div>
+      <ion-accordion v-for="course in getCourses" :key="course">
+        <recipe-course-item :courseName="`${course}`"></recipe-course-item>
       </ion-accordion>
     </ion-accordion-group>
   </base-card>
@@ -28,7 +36,10 @@ export default {
     RecipeCourseItem,
   },
   computed: {
-    ...mapGetters("recipes", ["getCategories"]),
+    ...mapGetters("recipes", ["getCourses"]),
+    hasCourses() {
+      return this.$store.getters["recipes/getCourses"];
+    },
   },
 };
 </script>
