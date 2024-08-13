9aec89e76a43a68bab4e96be31fe243114f8d3e0
jdimichele <justindimichele@gmail.com>
2024-08-13 11:09:20 -0700

    Rerouted some folder structures for broken TailwindCSS components, cleaned up some CSS and more.

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ import "@ionic/vue/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+import "./theme/output.css";
 
 /* Imports for global components. */
 import BaseCard from "./components/ui/BaseCard.vue";
